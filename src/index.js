3149198be1bd332b9e55f10d0518244e8e5a9e13
stefanyapulgarWOF-LA <stefanya.pulgar@wof-la.com>
2023-07-10 16:49:31 -0400

    en prueba

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Pages/app.css';
-import App from './Pages/_app';
+import './pages/app.css';
+import App from './pages/_app';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
