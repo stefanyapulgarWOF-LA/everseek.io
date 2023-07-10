1bcac6e24dfd15716ae745ff9f2bbda46009d3f9
stefanyapulgarWOF-LA <stefanya.pulgar@wof-la.com>
2023-07-10 18:11:07 -0400

    cambios en config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  babel: {
+    configFile: ".babelrc"
+  },
   exportPathMap: async function() {
     return {
       "/": { page: "/" },
       "/about-us": { page: "/about-us" }
     };
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/about-us',
+        destination: '/about-us.html',
+      },
+    ];
   },
   images: {
     unoptimized: true,
