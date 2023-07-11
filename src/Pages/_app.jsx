36699d8ceece9506b31b7ad333d4067d7c4cd356
stefanyapulgarWOF-LA <stefanya.pulgar@wof-la.com>
2023-07-11 10:58:40 -0400

    useE

--- src/Pages/_app.jsx
+++ src/Pages/_app.jsx
@@ -3,8 +3,10 @@ import { LangProvider } from "../Lang/LangProvider";
 import { React, useState, useEffect, useRef, useLayoutEffect } from "react";
 import { PageContext } from "../Context/AllContext";
 import "./app.css";
+import { useRouter } from "next/router";
 
 function App({Component, pageProps}) {
+  let router = useRouter();
   // const Wrapper = ({ children }) => {
   //   const location = useLocation();
   //   useLayoutEffect(() => {
@@ -14,7 +16,9 @@ function App({Component, pageProps}) {
   //   }, [location.pathname]);
   //   return children;
   // };
-
+  useEffect(()=>{
+    router.push(window.location.href)
+},[])
   return (
     <PageContext>
       <LangProvider>
