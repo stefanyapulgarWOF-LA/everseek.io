6c6c05a53f70da18e094f064f59a91228633d434
stefanyapulgarWOF-LA <stefanya.pulgar@wof-la.com>
2023-07-11 10:37:23 -0400

    sin next

--- next.config.js
+++ next.config.js
@@ -12,7 +12,9 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/about-us',destination: '/about-us'}
+        source: '/about-us',
+        destination: '/about-us.html',
+      },
     ];
   },
   images: {
